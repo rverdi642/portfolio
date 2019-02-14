6d28b41f2e023de91df771170939254254a91d12
rverdi642 <rverdi642@gmail.com>
2019-02-14 15:12:51 -0600

    new repo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,103 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, { Component } from "react";
+
+//import axios from "axios";
+import "./App.css";
+import Navigation from "./components/Navigation";
+// import Sidebar from "./components/Navigation/Sidebar";
+// import MainContent from "./components/MainContent";
+import LandingPage from "./components/LandingPage";
+import { Container } from "reactstrap";
+import "typeface-montserrat";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {
+  faIgloo,
+  faPlusCircle,
+  faCopy,
+  faTrash,
+  faHome,
+  faUser,
+  faSignOutAlt,
+  faArrowLeft,
+  faArrowRight,
+  faQuoteRight,
+  faAt
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faIgloo,
+  faPlusCircle,
+  faCopy,
+  faTrash,
+  faHome,
+  faUser,
+  faSignOutAlt,
+  faArrowLeft,
+  faArrowRight,
+  faQuoteRight,
+  faAt,
+  fab
+);
 
 class App extends Component {
+  // state = {
+    
+  //   loading: false,
+  //   loaded: false
+  // };
+
+
+  // handleClick = e => {
+  //   this.setState({ testLogin: false });
+  // };
+
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+   
+      return (
+        
+        <div>
+          <Container fluid>
+              <LandingPage handleClick={this.handleClick} />
+              <Navigation class="navbar-transparent" />
+              {/* <Sidebar /> */}
+              {/* <MainContent /> */}
+          </Container>
+        </div>
+        
+      );
+
   }
 }
 
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  Nav,
+  Container
+} from "reactstrap";
+import "./index.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" light expand="md">
+          <Container>
+            <NavbarBrand href="/">
+            <FontAwesomeIcon 
+                icon={['fas', 'at']}
+                class="space"
+            />
+           Portfolio
+            </NavbarBrand>
+            <Nav>
+              <NavItem>
+                <NavLink disabled href="#">
+                  {/* <p className="greeting">Hello! {this.props.user.username}</p> */}
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
